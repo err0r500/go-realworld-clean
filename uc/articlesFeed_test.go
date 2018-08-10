56b00dcd6988e1d59c29c4435e722cfa479362a7
Matthieu Jacquot <matthieu.jacquot@schibsted.com>
2018-08-10 15:49:43 +0200

    test uc/articlesFeed_test uc/articlesRecent_test

--- uc/articlesFeed_test.go
+++ uc/articlesFeed_test.go
@@ -3,6 +3,8 @@ package uc_test
 import (
 	"testing"
 
+	"errors"
+
 	"github.com/err0r500/go-realworld-clean/domain"
 	"github.com/err0r500/go-realworld-clean/implem/uc.mock"
 	"github.com/err0r500/go-realworld-clean/testData"
@@ -50,4 +52,57 @@ func TestInteractor_ArticlesFeed_happycases(t *testing.T) {
 		assert.Equal(t, rick, *user)
 	})
 
+	t.Run("empty count", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		i := mock.NewMockedInteractor(mockCtrl)
+		user, _, count, err := i.GetUCHandler().ArticlesFeed(rick.Name, -2, 0)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, count)
+		assert.Nil(t, user)
+	})
+}
+
+func TestInteractor_ArticlesFeed_fails(t *testing.T) {
+	mutations := map[string]mock.Tester{
+		"shouldPass": {
+			Calls: func(i *mock.Interactor) { // change nothing
+			},
+			ShouldPass: true},
+		"error return on uRW.GetByName": {
+			Calls: func(i *mock.Interactor) {
+				i.UserRW.EXPECT().GetByName(gomock.Any()).Return(nil, errors.New(""))
+			}},
+		"error return on GetByAuthorsNameOrderedByMostRecentAsc": {
+			Calls: func(i *mock.Interactor) {
+				i.ArticleRW.EXPECT().GetByAuthorsNameOrderedByMostRecentAsc(gomock.Any()).Return(nil, errors.New(""))
+			}},
+	}
+	rick := testData.User("rick")
+	rick.FollowIDs = []string{testData.User("jane").Name}
+	expectedArticles := domain.ArticleCollection{art1, art2, art3, art4}
+
+	// same as the happy case but with any parameter and called any number of times (including 0)
+	validCalls := func(i *mock.Interactor) {
+		i.UserRW.EXPECT().GetByName(gomock.Any()).Return(&rick, nil).AnyTimes()
+		i.ArticleRW.EXPECT().GetByAuthorsNameOrderedByMostRecentAsc(gomock.Any()).Return(expectedArticles, nil).AnyTimes()
+	}
+
+	for testName, mutation := range mutations {
+		t.Run(testName, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			i := mock.NewMockedInteractor(mockCtrl)
+			mutation.Calls(&i) // put the tested call first (important)
+			validCalls(&i)     // then fill the gaps with valid calls
+
+			_, _, _, err := i.GetUCHandler().ArticlesFeed(rick.Name, 2, 0)
+			if mutation.ShouldPass {
+				assert.NoError(t, err)
+				return
+			}
+			assert.Error(t, err)
+		})
+	}
 }
--- uc/articlesRecent_test.go
+++ uc/articlesRecent_test.go
@@ -3,6 +3,8 @@ package uc_test
 import (
 	"testing"
 
+	"errors"
+
 	"github.com/err0r500/go-realworld-clean/domain"
 	"github.com/err0r500/go-realworld-clean/implem/uc.mock"
 	"github.com/err0r500/go-realworld-clean/testData"
@@ -19,7 +21,7 @@ func TestInteractor_GetArticles(t *testing.T) {
 		defer mockCtrl.Finish()
 
 		offset := 2
-		filters := uc.NewFilters("jane", "", "")
+		filters := uc.NewFilters("jane", "bla", "true")
 		testUser := testData.User("rick")
 		i := mock.NewMockedInteractor(mockCtrl)
 		i.ArticleRW.EXPECT().GetRecentFiltered(filters).Return(expectedArticles, nil).Times(1)
@@ -30,6 +32,61 @@ func TestInteractor_GetArticles(t *testing.T) {
 		assert.Equal(t, 4, count)
 		assert.Equal(t, expectedArticles[offset:], articles)
 		assert.Equal(t, testData.User("rick"), *user)
-
 	})
+
+	t.Run("empty", func(t *testing.T) {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+
+		testUser := testData.User("rick")
+		i := mock.NewMockedInteractor(mockCtrl)
+
+		user, _, count, err := i.GetUCHandler().GetArticles(testUser.Name, -10, 2, nil)
+		assert.NoError(t, err)
+		assert.Nil(t, user)
+		assert.Equal(t, 0, count)
+	})
+}
+
+func TestInteractor_ArticlesRecent_fails(t *testing.T) {
+	expectedArticles := domain.ArticleCollection{art1, art2, art3, art4}
+
+	mutations := map[string]mock.Tester{
+		"shouldPass": {
+			Calls: func(i *mock.Interactor) { // change nothing
+			},
+			ShouldPass: true},
+		"error return on uRW.GetByName": {
+			Calls: func(i *mock.Interactor) {
+				i.UserRW.EXPECT().GetByName(gomock.Any()).Return(nil, errors.New(""))
+			}},
+		"error return on GetByAuthorsNameOrderedByMostRecentAsc": {
+			Calls: func(i *mock.Interactor) {
+				i.ArticleRW.EXPECT().GetRecentFiltered(gomock.Any()).Return(nil, errors.New(""))
+			}},
+	}
+	testUser := testData.User("rick")
+
+	// same as the happy case but with any parameter and called any number of times (including 0)
+	validCalls := func(i *mock.Interactor) {
+		i.ArticleRW.EXPECT().GetRecentFiltered(gomock.Any()).Return(expectedArticles, nil).AnyTimes()
+		i.UserRW.EXPECT().GetByName(gomock.Any()).Return(&testUser, nil).AnyTimes()
+	}
+
+	for testName, mutation := range mutations {
+		t.Run(testName, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+			i := mock.NewMockedInteractor(mockCtrl)
+			mutation.Calls(&i) // put the tested call first (important)
+			validCalls(&i)     // then fill the gaps with valid calls
+
+			_, _, _, err := i.GetUCHandler().GetArticles(testUser.Name, 10, 2, nil)
+			if mutation.ShouldPass {
+				assert.NoError(t, err)
+				return
+			}
+			assert.Error(t, err)
+		})
+	}
 }
